0054cdfdc1aba2ef725488e80451546d3f707826
Harris Robin Kalash <hello@harris.dev>
2023-01-19 10:38:12 -0500

    pretty print the output

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,27 @@
-use clap::{App, Arg};
+use std::collections::HashMap;
+
+use clap::{Arg, ArgAction, Command};
 use whois_rust::{WhoIs, WhoIsLookupOptions};
 
 fn main() {
     let whois = WhoIs::from_path("./servers.json").unwrap();
 
-    let matches = App::new("Whoopty")
-    .version("1.0")
-    .author("Harris Robin Kalash <hello@harris.dev>")
-    .about("Whoopty is a CLI tool for looking up WHOIS information for domains")
-    .arg(Arg::with_name("domain")
-        .short('d')
-        .long("domain")
-        .value_name("DOMAIN")
-        .help("Sets the domain to lookup")
-        .takes_value(true))
-    .get_matches();
+    let matches = Command::new("Whoopty")
+        .version("1.0")
+        .author("Harris Robin Kalash <hello@harris.dev>")
+        .about("Whoopty is a CLI tool for looking up WHOIS information for domains")
+        .arg(Arg::new("domain")
+            .short('d')
+            .long("domain")
+            .value_name("DOMAIN")
+            .action(ArgAction::Set)
+            .help("Sets the domain to lookup")
+        ).get_matches();
 
     // get the value of the input argument
-    let domain =
-        matches.value_of("domain").unwrap_or("google.com");
+    let domain = matches
+        .get_one::<String>("domain")
+        .expect("No domain provided");
 
     let result: String = whois
         .lookup(
@@ -27,5 +30,31 @@ fn main() {
         )
         .unwrap();
 
-    println!("{:?}", result);
+    let seperated = separate_sentences(&result);
+
+    println!("{:#?}", seperated);
+}
+
+fn separate_sentences(
+    text: &str,
+) -> HashMap<String, String> {
+    let mut sentences = HashMap::new();
+
+    for line in text.lines() {
+        let colon_index = match line.find(':') {
+            Some(index) => index,
+            None => 0,
+        };
+
+        if !line.is_empty() {
+            let (before, after) =
+                line.split_at(colon_index);
+            sentences.insert(
+                before.trim().to_string(),
+                after[1..].trim().to_string(),
+            );
+        }
+    }
+
+    sentences
 }
